f6946f612f1a04597203e8e89c34752fd71270f7
Jody Chambers <jody.a.chambers@gmail.com>
2015-06-05 21:39:43 +0000

    added location for each picture

--- main.js
+++ main.js
@@ -2,13 +2,31 @@ $(function() {
   // Grab DOM elements that I will work with
   var $carouselInner = $(".carousel-inner");
   var $carouselIndicators = $(".carousel-indicators");
-  var randNum = Math.floor((Math.random() * 1000000000) + 1);
+  var randNum = Math.floor((Math.random() * 100000000) + 1);
+  console.log(randNum);
   var url = 'https://api.instagram.com/v1/users/' + randNum + '/media/recent/?access_token=357844946.7fa8ffd.f8c258ae549d4ef6bb0a3038b2444b7d';
+
+  function addGuessNameButton(response){
+    $('<button>').addClass('btn btn-primary guessName').text('Guess Name').appendTo('.container');
+    $('.guessName').click(function(){
+      var $this = $(this);
+      if($this.hasClass('guessName')){
+        $this.removeClass('guessName');
+        $this.text('Guess Name');         
+      } else {
+        $this.addClass('guessName');
+        if ( response.data[0].user.full_name != "" ){
+          $this.text(response.data[0].user.full_name);
+        }else {
+          $this.text(response.data[0].user.username);
+        }
+      }
+    });
+  }
 
   // Request products from API
   $.ajax({
     url: url,
-    jsonp: 'callback',
     dataType: 'jsonp',
     data: {
       format: 'json' 
@@ -17,38 +35,40 @@ $(function() {
       console.log(response);
       var ctr = 0;
 
-      response.data.forEach(function(photo) {
-        var tagCtr = 0;
-        $item = $("<div>").addClass("item").attr('id', ctr);
-        $("<img>").addClass("photo-img").attr('src', photo.images.standard_resolution.url).appendTo($item);
-        $caption = $('<div>').addClass('carousel-caption').appendTo($item);
-        photo.tags.forEach(function(tag) {
-          if (tagCtr < 3) {
-            $('<span class="label label-info">' + tag + '</span>').appendTo($caption);
+      if (response.data == undefined || response.data.length == 0) {
+        location.reload();
+      } else {
+        addGuessNameButton(response);
+        response.data.forEach(function(photo) {
+          var tagCtr = 0;
+          $item = $("<div>").addClass("item").attr('id', ctr);
+          $("<img>").addClass("photo-img").attr('src', photo.images.standard_resolution.url).appendTo($item);
+          $caption = $('<div>').addClass('carousel-caption').appendTo($item);
+          $captionTags = $('<div>').addClass('caption-tags').appendTo($caption);
+          photo.tags.forEach(function(tag) {
+            if (tagCtr < 3) {
+              $('<span class="label label-info">' + tag + '</span>').appendTo($captionTags);
+            }
+            tagCtr++;
+          });
+          if ( photo.location != null ){
+            var lat = photo.location.latitude;
+            var lng = photo.location.longitude;
+            var latLng = "http://maps.google.com/maps?q=" + lat + "," + lng;
+            $mapLink = $('<a>').attr("href", latLng).attr("target", "_blank");
+            $('<button>').addClass('btn-xs btn-success guessName').text('Guess Location').appendTo($mapLink);
+            $mapLink.appendTo($caption);
           }
-          tagCtr++;
-        })
-        $item.appendTo('.carousel-inner');
-        $carouselIndicators.append('<li class="ind" data-target="#carousel-example-generic" data-slide-to="' + ctr + '"></li>');
-        ctr++;
-      });
-      $('#0').addClass('active');
-      console.log("Data loaded");
-    }
-  });
 
-  // Set up event listener for products in aside
-  // console.log("setting up event listener for click .photo");
-  // $(".photo-list").on('click', ".photo", function() {
-  //   // Grab the div.photo that was clicked
-  //   var $photo = $(this);
-  //   var photoData = $photo.data('photo');
-  //   console.log(photoData);
-  //   $(".welcome").hide();
-  //   $(".photo-info").show();
-  //   $(".photo-info h2.name").text(photoData.name);
-  //   $(".photo-info img.image").attr('src', photoData.image_thumb_url);
-  // });
+          $item.appendTo('.carousel-inner');
+          $carouselIndicators.append('<li class="ind" data-target="#carousel-example-generic" data-slide-to="' + ctr + '"></li>');
+          ctr++;
+        });
+        $('#0').addClass('active');
+        console.log("Data loaded");
+      }
+    } // end of else
+  });
 
 });
 

bc15ccd2966ad6ee74961f47c357033345609902
Jody Chambers <jody.a.chambers@gmail.com>
2015-06-07 04:54:50 +0000

    added profilePic modal

--- main.js
+++ main.js
@@ -8,29 +8,45 @@ $(function() {
 
   function addGuessNameButton(response){
     $('<button>').addClass('btn btn-primary guessName').text('Guess Name').appendTo('.container');
+    addProfilePic(response);
     $('.guessName').click(function(){
       var $this = $(this);
       if($this.hasClass('guessName')){
-        $this.removeClass('guessName');
-        $this.text('Guess Name');         
-      } else {
-        $this.addClass('guessName');
         if ( response.data[0].user.full_name != "" ){
           $this.text(response.data[0].user.full_name);
         }else {
           $this.text(response.data[0].user.username);
         }
+        $this.removeClass('guessName');        
+      } else {
+        $this.addClass('guessName');
+        $this.text('Guess Name');
       }
     });
+  }
+
+  function addProfilePic(response){
+    $profilePic = $('<a>').attr('href', '#').attr('data-toggle','modal').attr('data-target','#profilePicModal');
+    $('<img>').addClass('profilePic').attr('src',response.data[0].user.profile_picture).appendTo($profilePic);
+    $modalDiv = $('<div>').addClass('modal fade profilePicModal')
+      .attr('aria-hidden', 'true')
+      .attr('aria-labelledby', 'profilePicModal')
+      .attr({
+        id: 'profilePicModal',
+        tabindex: '-1',
+        role: 'dialog'
+      });
+    $modalDialog = $('<div>').addClass('modal-dialog modal-sm').appendTo($modalDiv);
+    $modalContent = $('<div>').addClass('modal-content').appendTo($modalDialog);
+    $('<img>').addClass('modal-content').attr('src',response.data[0].user.profile_picture).appendTo($modalContent);
+    $modalDiv.appendTo('.container');
+    $profilePic.appendTo('.container');
   }
 
   // Request products from API
   $.ajax({
     url: url,
     dataType: 'jsonp',
-    data: {
-      format: 'json' 
-    },
     success: function(response) {
       console.log(response);
       var ctr = 0;
@@ -54,7 +70,7 @@ $(function() {
           if ( photo.location != null ){
             var lat = photo.location.latitude;
             var lng = photo.location.longitude;
-            var latLng = "http://maps.google.com/maps?q=" + lat + "," + lng;
+            var latLng = "http://maps.google.com/maps/place/@" + lat + "," + lng + ',5z/data=!4m2!3m1!1s0x0:0x0!5m1!1e4!6m1!1e1';
             $mapLink = $('<a>').attr("href", latLng).attr("target", "_blank");
             $('<button>').addClass('btn-xs btn-success guessName').text('Guess Location').appendTo($mapLink);
             $mapLink.appendTo($caption);
@@ -66,7 +82,7 @@ $(function() {
         });
         $('#0').addClass('active');
         console.log("Data loaded");
-      }
+        }
     } // end of else
   });
 
